da5c96b5fc33a56f0259962495f565abbdd6b051
Muhammad Tayyab Ali <tayyab.ali@fiveriverstech.com>
2023-03-01 14:03:44 +0500

    Initial commit

---
+++ src/utils/layers.js
@@ -0,0 +1,80 @@
+export const dataLayer = {
+  id: 'data',
+  type: 'fill',
+  paint: {
+    'fill-color': {
+      property: 'percentile',
+      stops: [
+        [0, '#3288bd'],
+        [1, '#66c2a5'],
+        [2, '#abdda4'],
+        [3, '#e6f598'],
+        [4, '#ffffbf'],
+        [5, '#fee08b'],
+        [6, '#fdae61'],
+        [7, '#f46d43'],
+        [8, '#d53e4f']
+      ]
+    },
+    'fill-opacity': 0.8
+  }
+};
+
+export const clusterLayer = {
+  id: 'clusters',
+  type: 'circle',
+  source: 'earthquakes',
+  filter: ['has', 'point_count'],
+  paint: {
+    'circle-color': ['step', ['get', 'point_count'], '#51bbd6', 100, '#f1f075', 750, '#f28cb1'],
+    'circle-radius': ['step', ['get', 'point_count'], 20, 100, 30, 750, 40]
+  }
+};
+
+export const clusterCountLayer = {
+  id: 'cluster-count',
+  type: 'symbol',
+  source: 'earthquakes',
+  filter: ['has', 'point_count'],
+  layout: {
+    'text-field': '{point_count_abbreviated}',
+    'text-font': ['DIN Offc Pro Medium', 'Arial Unicode MS Bold'],
+    'text-size': 12
+  }
+};
+
+export const unclusteredPointLayer = {
+  id: 'unclustered-point',
+  type: 'circle',
+  source: 'earthquakes',
+  filter: ['!', ['has', 'point_count']],
+  paint: {
+    'circle-color': '#11b4da',
+    'circle-radius': 4,
+    'circle-stroke-width': 1,
+    'circle-stroke-color': '#fff'
+  }
+};
+
+export const countiesLayer = {
+  id: 'counties',
+  type: 'fill',
+  'source-layer': 'original',
+  paint: {
+    'fill-outline-color': 'rgba(0,0,0,0.1)',
+    'fill-color': 'rgba(0,0,0,0.1)'
+  }
+};
+
+// Highlighted county polygons
+export const highlightLayer = {
+  id: 'counties-highlighted',
+  type: 'fill',
+  source: 'counties',
+  'source-layer': 'original',
+  paint: {
+    'fill-outline-color': '#484896',
+    'fill-color': '#6e599f',
+    'fill-opacity': 0.75
+  }
+};
